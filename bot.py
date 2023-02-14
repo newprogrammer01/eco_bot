0dd7e2ac203acc2ac83ce3adb6f10faebd9f3fec
newprogrammer01 <jasurbekpirnazarov09gmail.com>
2023-02-14 11:00:52 +0500

    webhook

--- bot.py
+++ bot.py
@@ -1,29 +1,11 @@
-from flask import Flask, request, jsonify
-import os
-import telegram
-
-
-app = Flask(__name__)
-TOKEN=os.environ.get('TOKEN')
-bot=telegram.Bot(token='6047203124:AAEHAsliWgI329nLtH5vv8ymWKHmRQ-6V8s')
-
-@app.route('/')
-def home():
-    html = '''
-    <h1> This is a home page </h1>
-    <p> This is a paragraph </p>
-    '''
-    return html
-
-
-@app.route('/api', methods=['POST'])
-def api():
-
-    data = request.get_json(force=True)
-    output = {'message': 'Hello World'}
-    return output
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, request
+import requests
+import os 
+app=Flask(__name__)
+TOKEN=os.environ['TOKEN']
+@app.route('webhook', methods=['POST'])
+def webhook():
+    data=request.get_json(force=True)
+    print(data)
+    return 'ok'
+    
